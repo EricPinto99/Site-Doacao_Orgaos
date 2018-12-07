701c108b260dace66177eed5e43db1f12f9b7713
Éric Pinto <ericgrandopinto@gmail.com>
2018-12-07 00:33:15 -0200

    feat: added some files created on the page

---
+++ scripts.js
@@ -0,0 +1,25 @@
+sliderAtual = 1;
+sliderMax = 3;
+tempoSlider = 3000;
+
+function troca() {
+	document.getElementById('b1').style.visibility = "hidden";
+	document.getElementById('b2').style.visibility = "hidden";
+	document.getElementById('b3').style.visibility = "hidden";
+
+	document.getElementById('b' + sliderAtual).style.visibility = "visible";
+
+	sliderAtual = sliderAtual+1;
+
+	if (sliderAtual > sliderMax) {
+		sliderAtual = 1;
+	}
+}
+
+function slider(){
+	document.getElementById('b1').style.visibility = "visible";
+	document.getElementById('b2').style.visibility = "hidden";
+	document.getElementById('b3').style.visibility = "hidden";
+
+	sliderTimer = setInterval(troca,tempoSlider);
+}
